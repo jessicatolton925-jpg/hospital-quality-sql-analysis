63d70872c394c6f1e7581996ce661365d19b7e39
jessicatolton925-jpg <jessica.tolton925@gmail.com>
2025-11-14 19:30:04 -0500

    Update 02_mortality_readmissions_join.sql

--- sql/02_mortality_readmissions_join.sql
+++ sql/02_mortality_readmissions_join.sql
@@ -1,16 +1,11 @@
 -- 02_mortality_readmissions_join.sql
--- Author: Jessica Tolton
--- Description: Joins hospital data with mortality and readmission datasets
---              to create a unified quality evaluation table.
 
-CREATE OR REPLACE TABLE `project.hospital_quality_joined` AS
+CREATE OR REPLACE TABLE `sql-portfolio-project-478121.healthcare_data.mortality_readmissions_join` AS
 SELECT
-  h.hospital_name,
-  h.state,
-  m.mortality_score,
-  r.readmission_score
-FROM `project.cleaned_hospital_data` h
-LEFT JOIN `project.mortality_data` m
-    ON h.hospital_name = m.hospital_name
-LEFT JOIN `project.readmissions_data` r
-    ON h.hospital_name = r.hospital_name;
+  m.provider_id,
+  AVG(m.mortality_score) AS avg_mortality_score,
+  AVG(r.readmission_score) AS avg_readmission_score
+FROM `sql-portfolio-project-478121.healthcare_data.mortality_clean` m
+LEFT JOIN `sql-portfolio-project-478121.healthcare_data.readmission_clean` r
+ON m.provider_id = r.provider_id
+GROUP BY m.provider_id;
