c33906fd1e63739fbe27f2af1eeb3662d9a62459
jessicatolton925-jpg <jessica.tolton925@gmail.com>
2025-11-14 19:33:35 -0500

    Update 06_state_level_summary.sql

--- sql/06_state_level_summary.sql
+++ sql/06_state_level_summary.sql
@@ -1,16 +1,9 @@
 -- 06_state_level_summary.sql
--- Author: Jessica Tolton
--- Description: Produces state-level summary metrics, including:
---              - Average combined quality score
---              - Hospital count per state
 
-CREATE OR REPLACE TABLE `project.state_level_summary` AS
+CREATE OR REPLACE TABLE `sql-portfolio-project-478121.healthcare_data.state_quality_scores` AS
 SELECT
   state,
-  ROUND(AVG(combined_quality_score), 2) AS avg_quality_score,
-  COUNT(*) AS hospitals_in_state
-FROM `project.hospital_quality_scored`
+  ROUND(AVG(combined_quality_score), 2) AS avg_state_quality_score
+FROM `sql-portfolio-project-478121.healthcare_data.combined_quality_scores`
 GROUP BY state
-ORDER BY avg_quality_score DESC;
-
-SELECT * FROM `project.state_level_summary`;
+ORDER BY avg_state_quality_score DESC;
