381eac7609ebc6430b1205e45b18e0b11564f041
jessicatolton925-jpg <jessica.tolton925@gmail.com>
2025-11-14 19:30:23 -0500

    Update 03_combined_quality_score.sql

--- sql/03_combined_quality_score.sql
+++ sql/03_combined_quality_score.sql
@@ -1,16 +1,28 @@
 -- 03_combined_quality_score.sql
--- Author: Jessica Tolton
--- Description: Creates a combined quality score based on mortality + readmission metrics.
 
--- Weighted formula (example):
--- combined_quality_score = (mortality_score * 0.6) + (readmission_score * 0.4)
-
-CREATE OR REPLACE TABLE `project.hospital_quality_scored` AS
+CREATE OR REPLACE TABLE `sql-portfolio-project-478121.healthcare_data.combined_quality_scores` AS
 SELECT
-  hospital_name,
-  state,
-  mortality_score,
-  readmission_score,
-  ROUND((mortality_score * 0.6) + (readmission_score * 0.4), 2)
-    AS combined_quality_score
-FROM `project.hospital_quality_joined`;
+  h.provider_id,
+  h.hospital_name,
+  h.city,
+  h.state,
+  mrj.avg_mortality_score,
+  mrj.avg_readmission_score,
+  ec.effective_care_score,
+  pe.patient_exp_score,
+
+  -- Weighted quality score model
+  (
+    (mrj.avg_mortality_score * 0.35) +
+    (mrj.avg_readmission_score * 0.35) +
+    (ec.effective_care_score * 0.20) +
+    (pe.patient_exp_score * 0.10)
+  ) AS combined_quality_score
+
+FROM `sql-portfolio-project-478121.healthcare_data.hospitals_clean` h
+LEFT JOIN `sql-portfolio-project-478121.healthcare_data.mortality_readmissions_join` mrj
+  ON h.provider_id = mrj.provider_id
+LEFT JOIN `sql-portfolio-project-478121.healthcare_data.effective_care_clean` ec
+  ON h.provider_id = ec.provider_id
+LEFT JOIN `sql-portfolio-project-478121.healthcare_data.patient_exp_clean` pe
+  ON h.provider_id = pe.provider_id;
